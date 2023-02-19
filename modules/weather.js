888d18cc39845a747ab2516b2a281afeb7dc5154
Araceligm90 <garcia.araceli90@gmail.com>
2023-02-19 13:42:50 -0500

    cache added

--- modules/weather.js
+++ modules/weather.js
@@ -1,15 +1,29 @@
 'use strict';
 
 const axios = require('axios');
+const cache = require('../cache');
 
 function getWeather(req, res, next) {
   let city = req.query.searchQuery;
+  const key = 'weather ' + city;
   const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${process.env.WEATHER_API_KEY}&days=10`;
 
+
+  if (cache[key] && (Date.now() - cache[key].timestamp < 240000)) {
+    console.log('Weather Cache Hit');
+    res.status(200).send(cache[key].data);
+  }
+  else {
+    console.log('Weather Cache Miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+  }
   axios.get(weatherUrl)
-    .then(response => {
-      let liveInfo = response.data.data.map(day => new Forecast(day));
-      res.status(200).send(liveInfo);
+    .then(response => response.data.data.map(day => new Forecast(day)))
+    // saves cache data in formatted data and send it
+    .then(formattedData => {
+      cache[key].data = formattedData;
+      res.status(200).send(formattedData);
     })
     .catch(error => next(error));
 }
@@ -22,7 +36,7 @@ class Forecast {
   }
 }
 
-// OLD WAY BEFORE REFACTORING 
+// OLD WAY BEFORE REFACTORING
 
 // app.get('/weather', async (req, res) => {
 //   let city = req.query.searchQuery;
